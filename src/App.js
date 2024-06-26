1d225edc46f2d052b4675316f7d818950d4620c5
Jona710than <jfnde710@gmail.com>
2024-06-26 13:32:52 +0100

    commiut

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import './App.css';
 function App() {
   return (
     <>
-    <div class="bg-red-500">
-      
-    <h1 class="bg-white-500  w-20 p-6">
-          Hello world!
-        </h1>
-        <h1 class="bg-white w-20 p-6">
-         world wide pluto!
-      </h1>
-      <p class="m">jonathan</p>
-    </div>
+     <div class="float-right">
+   <div class="bg-green-500 flex w-72">
+            
+          <h1 class="  w-20 p-6">
+                Hello world!
+              </h1>
+              <h1 class="bg-white w-20 p-6 ">
+              world wide web!
+            </h1>
+            <p class="w-20 p-6">web development</p>
+          </div>
+      </div>  
     </>
   );
 }
